05e67917962ac4ebde3bd439ab894fbedff1e9f0
emfo2405 <emfo2405@student.miun.se>
2025-05-29 20:09:42 +0200

    Funktion skapad för att registrera användare

--- routes/regRoutes.js
+++ routes/regRoutes.js
@@ -8,7 +8,24 @@ const jwt = require("jsonwebtoken");
 
 //Lägga till en användare
 router.post("/register", async(req, res) => {
-    console.log("Registrering påbörjad");
+    try {
+        const { username, password, email } = req.body;
+
+        //Validera input
+        if(!username || username.length < 6) {
+            return res.status(400).json({error: "Användarnamn ska vara ifyllt och längre än 5 tecken"})
+        } else if(!password || password.length < 9) {
+            return res.status(400).json({error: "Lösenord ska vara ifyllt och längre än 8 tecken"})
+        } else if(!email) {
+            return res.status(400).json({error: "E-post ska vara ifyllt"})
+        }
+
+        res.status(201).json({message: "Användare skapad"});
+        
+
+    } catch {
+        res.status(500).json({error: "Server error"});
+    }
 });
 
 //Logga in en användare
