b47bcab826b9cf657d3c3fc25cc2e25f8ba25756
emfo2405 <emfo2405@student.miun.se>
2025-05-29 20:58:30 +0200

    Skapad route för hemlig sida samt validering av token

--- server.js
+++ server.js
@@ -15,6 +15,26 @@ const port = process.env.PORT || 3001;
 //Routes
 app.use("/api", regRoutes);
 
+//Skapa en skyddad route
+app.get("/api/secret", authenticateToken, (req, res) => {
+    res.json({message: "skyddad route"})
+});
+
+//Validera token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null ) res.status(401).json({message: "Ingen tillgång till denna sida - token saknas"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if(err) return res.status(403).json({message: "Ogiltig JWT"})
+
+            req.username = user.username;
+            next();
+    });    
+}
+
 //Starta applikationen
 app.listen(port, () => {
     console.log("Server running at http://localhost:" + port);
